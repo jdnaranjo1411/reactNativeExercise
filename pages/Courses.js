dc13f215c916831cf3b22608fac99ccfff1b408e
jdnaranjo1411 <112138324+jdnaranjo1411@users.noreply.github.com>
2023-10-05 12:34:09 -0500

    se cargo por fin la api
    api cargada y funcionando en consola

--- pages/Courses.js
+++ pages/Courses.js
@@ -1,23 +1,39 @@
-import { StyleSheet, Text, View } from "react-native-web";
-import React from "react";
-import { Button } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, FlatList, Button, StyleSheet } from "react-native";
 import axios from "axios";
 
 export default function Courses({ navigation }) {
-  
-  axios.get("http://localhost:3001/api/v1/getCursos").then((res) => {
-    console.log(res.data);
-  });
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3002/api/v1/getCursos")
+      .then((res) => {
+        setCourses(res.data);
+      })
+      .catch((error) => {
+        console.log("Error al obtener los cursos:", error);
+      });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text></Text>
+      <Text>Lista de Cursos</Text>
+      <FlatList
+        data={courses}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.courseItem}>
+            <Text style={styles.courseName}>{item.nombre_curso}</Text>
+            <Text style={styles.courseDescription}>{item.descripcion_curso}</Text>
+          </View>
+        )}
+      />
       <Button
-        title="HomePage_Buttom"
+        title="Ir a la página de inicio"
         onPress={() => navigation.navigate("HomePage")}
       />
-
       <Button
-        title="Students_Buttom"
+        title="Ir a la página de estudiantes"
         onPress={() => navigation.navigate("Students")}
       />
     </View>
@@ -30,5 +46,18 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  courseItem: {
+    marginVertical: 10,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "#ccc",
+  },
+  courseName: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  courseDescription: {
+    fontSize: 14,
   },
 });
